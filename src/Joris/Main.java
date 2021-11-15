51a5672c914df5a16e2702f9e5d88e1e36786c8f
Kilian <Kilian@DESKTOP-MBJ82TI>
2021-11-15 15:10:39 +0100

    premierCoup + Raccordement de fonctions

--- src/Joris/Main.java
+++ src/Joris/Main.java
@@ -3,10 +3,7 @@ package Joris;
 public class Main {
 	public static void main(String[] args) {
 		Strategy strat = new Strategy();
-		//strat.avanceVersPalet();
-		//strat.getJoris().tourner(180);
-		strat.reperage();
-		//strat.getJoris().getPilot().rotate(190,false);
+		strat.premierCoup();
 	}
 	
 	
